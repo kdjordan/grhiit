d1d167a0fa79f557eb3f98ac679ef36ea3991062
kdjordan <k.dean.jordan@gmail.com>
2022-11-03 21:03:22 -0700

    added tailwind

---
+++ grhiit-client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+        <div className="flex h-screen content-between">
+            <div>grHiit</div>
+            <div>links</div>
+        </div>
+    )
+}
