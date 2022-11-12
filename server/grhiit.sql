25879c05464058c5841ede97cfec8b12b35fb520
kdjordan <k.dean.jordan@gmail.com>
2022-11-12 09:57:21 -0800

    db config updates

---
+++ server/grhiit.sql
@@ -0,0 +1,18 @@
+\echo 'Delete and recreate grhiit db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE grhiit;
+CREATE DATABASE grhiit;
+\connect grhiit
+
+\i grhiit-schema.sql
+\i grhiit-seed.sql
+
+\echo 'Delete and recreate grhiit_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE grhiit_test;
+CREATE DATABASE grhiit_test;
+\connect grhiit_test
+
+\i grhiit-schema.sql
