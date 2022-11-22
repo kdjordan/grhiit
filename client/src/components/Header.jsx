07c83c981a9a7d3a7d92fa1270aca2462920b673
kdjordan <k.dean.jordan@gmail.com>
2022-11-21 21:08:20 -0800

    nav cleanup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,62 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default function Header() {
     // const [open, cycleOpen] = useCycle(false, true);
     const [open, setOpen] = useState(false)
     
+    useEffect(() => {
+        function handleResize() {
+          if (window.innerWidth > 640) {
+            setOpen(false)
+          }
+        }
+        window.addEventListener('resize', handleResize)
+    })
 
 
     function toggleMenu() {
-        console.log('togglin ', open)
         setOpen(o => !o)
     }
     
     // if(!isLoggedIn) {
         return (
-            <nav className="nav bg-grred p-2 z-1 w-full flex flex-col justify-content-center">
-                <div className="container mx-auto flex items-center justify-between">
+            <nav className={`nav bg-grred p-2 z-1 w-full flex flex-col justify-content-center`}>
+                <div className={`container mx-auto flex items-center justify-between pb-1`}>
                     {/* grhiit */}
-                    <div className="text-grwhite font-osPrimary hover:text-grgrey hover:no-underline text-2xl pl-2">
+                    <div className="text-grwhite font-osPrimary hover:text-grgrey hover:no-underline text-3xl">
                         <a className="no-underline" href="#">GRHIIT</a>
                     </div>
                     {/* button */}
-                    <div>
-                        <button 
-                            className="text-3xl md:hidden focus:outline-none text-grwhite cursor-pointer self-end" 
-                            onClick={toggleMenu}
-                        >
-                            {open ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                    </div>
+                    <button 
+                        className="md:hidden focus:outline-none text-grwhite cursor-pointer self-end text-4xl" 
+                        onClick={toggleMenu}
+                    >
+                        {open ? <>&times;</> : <>&#9776;</>}
+                    </button>
                     {/* not mobile */}
                     <div className="md:flex pt-2 items-center justify-between font-osPrimary hidden">
                         <ul className="list-none md:flex justify-between items-center hidden">
