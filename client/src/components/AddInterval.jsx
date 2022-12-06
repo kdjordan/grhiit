320203bb09958d1a85043720fe03dcf65649d051
kdjordan <k.dean.jordan@gmail.com>
2022-12-05 16:00:31 -0800

    interval builder scaffolded

---
+++ client/src/components/AddInterval.jsx
@@ -0,0 +1,140 @@
+import { useState } from 'react'
+
+export default function AddInterval({ addInterval }) {
+    const [form, setForm] = useState({
+        movement: '',
+        abbreviation: '',
+        work: '',
+        rest: '',
+        rounds: ''
+    })
+    const [errors, setErrors] = useState([])
+
+    function handleChange(e) {
+        console.log(e.target)
+        const { name, value } = e.target
+        setForm(f => ({
+            ...f,
+            [name]: value
+        })) 
+      }
+
+      async function handleSubmit(e) {
+        // Prevent default form submission
+        e.preventDefault()
+        addInterval(form)
+      }
+
+      const handleFocus = (event) => {
+        // Clear errors for the input field that was focused
+        setErrors([])
+      }
+
+      const preventMinus = (e) => {
+        if (e.code === 'Minus') {
+            e.preventDefault();
+        }
+    };
+
+    return (
+        <div className="flex flex-col px-2 mt-4 mb-8 w-full lg:w-2/3">
+        <div className="bg-grblack text-lg px-6 py-4 rounded shadow-md text-grgrey">
+            <h4 className="text-2xl text-center pb-4">ADD AN INTERVAL</h4>
+            {errors.length ? 
+                <div className="text-center text-grred pb-4 text-xl">
+                    <ul>
+                        {errors.map((e, index) => {
+                            return <li key={index}>{e}</li>
+                        })}
+
+                    </ul>
+                </div> 
+                : ''}
+                <form onSubmit={handleSubmit}>
+                    <div className="flex align-center gap-8">
+                        <div className="flex flex-col align-start w-full">
+                            <label className="block text-grwhite text-left mb-2 text-bae" 
+                                htmlFor="firstName">MOVEMENT NAME
+                            </label>
+                            <input 
+                                type="text"
+                                className="block border bg-transparent border-grred  w-full p-3 rounded mb-4"
+                                name="movement"
+                                value={form.movement}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                                required
+                                placeholder="Movement" />
+                        </div>
+                        <div className="flex flex-col align-start w-full">
+                            <label className="block text-grwhite text-left mb-2" 
+                                htmlFor="lastName">ABBREVIATION
+                            </label>
+                            <input 
+                                type="text"
+                                className="block border bg-transparent border-grred  w-full p-3 rounded mb-4"
+                                name="abbreviation"
+                                value={form.abbreviation}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                                required
+                                placeholder="MAX 8 Letters" />
+                        </div>
+                    </div>
+                    <div className="flex align-center gap-8">
+                        <div className="flex flex-col align-start w-full">
+                            <label className="block text-grwhite text-left mb-2" 
+                                htmlFor="password">Work
+                            </label>
+                            <input 
+                                type="number"
+                                className="block border bg-transparent border-grred  w-full p-3 rounded mb-4"
+                                name="work"
+                                value={form.work}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                                onKeyPress={preventMinus}
+                                placeholder="Seconds" />
+                        </div>
+                        <div className="flex flex-col align-start w-full">
+                            <label className="block text-grwhite text-left mb-2" 
+                                htmlFor="rest">Rest
+                            </label>
+                            <input 
+                                type="number"
+                                className="block border bg-transparent border-grred  w-full p-3 rounded mb-4"
+                                name="rest"
+                                value={form.rest}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                                onKeyPress={preventMinus}
+                                required
+                                placeholder="Seconds" />
+                        </div>
+                        <div className="flex flex-col align-start w-full">
+                            <label className="block text-grwhite text-left mb-2" 
+                                htmlFor="rounds">Rounds
+                            </label>
+                            <input 
+                                type="number"
+                                className="block border bg-transparent border-grred  w-full p-3 rounded mb-4"
+                                name="rounds"
+                                value={form.rounds}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                                onKeyPress={preventMinus}
+                                required
+                                placeholder="Rounds" />
+                        </div>
+                    </div>
+                    <div className="flex align-center">
+                        <button
+                            type="submit"
+                            className="w-1/3 mx-auto self-center text-center text-base sm:text-sm py-3 rounded bg-grred text-grwhite hover:bg-grwhite hover:text-grred duration-300 focus:outline-none my-1"
+                        >ADD INTERVAL</button>
+                    </div> 
+                </form>
+        </div>
+    </div>
+    )
+}
--- client/src/views/unauth/auth/Create.jsx
+++ client/src/views/unauth/auth/Create.jsx
@@ -1,5 +1,31 @@
+// import { useDrag, useDrop } from 'react-dnd';
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+import AddInterval from '../../../components/AddInterval'
+
 export default function Create() {
+    const [intervals, setIntervals] = useState([])
+
+    function addInterval(form) {
+        console.log('adding ', form)
+        setIntervals(int => [...int, form])
+    }
+
     return (
-        <h2 className="mt-32 text-grwhite">Create</h2>
+        <motion.div 
+        className="container mx-auto text-3xl md:text-5xl h-screen text-grwhite flex flex-col items-center mt-24"
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        exit={{opacity:0, transition: {duration: 0.5}}}        
+        >
+        <h1 className="mb-8 text-center text-grwhite w-full">TRAINING SESSION BUILDER</h1> 
+        <AddInterval addInterval={addInterval}/>
+        <h1 className="mb-8 text:xl md:text-3xl text-center text-grwhite w-full">INTERVALS</h1>
+        {intervals.length > 0 ? (
+            <h2>We have intervals</h2>
+        ) : (
+            <h2>No intervals :(</h2>
+        )}
+        </motion.div>
     )
 }
