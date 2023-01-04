5e16c398ae3a89df0a934b7121eb47bb5ee389eb
kdjordan <k.dean.jordan@gmail.com>
2023-01-03 21:03:26 -0800

    all tests passing

--- server/models/workout.js
+++ server/models/workout.js
@@ -16,7 +16,7 @@ class Workout {
    * */
   static async create(userId, obj) {
     const newData = JSON.stringify(obj.data)
-    
+
     const result = await db.query(
       `INSERT INTO workouts
        (user_id, name, description, data)
@@ -69,7 +69,6 @@ class Workout {
    * */
 
   static async getWorkout(id) {
-    console.log('getting workout ', id)
     const result = await db.query(
       `SELECT data 
         FROM workouts 
@@ -77,100 +76,12 @@ class Workout {
       [id]
     );
     
+    if (result.rows.length === 0) throw new NotFoundError(`No workout: ${id}`);
+    
     const workout = result.rows[0].data;
-  
     return workout;
   }
 
-  /** Given a company handle, return data about company.
-   *
-   * Returns { handle, name, description, numEmployees, logoUrl, jobs }
-   *   where jobs is [{ id, title, salary, equity }, ...]
-   *
-   * Throws NotFoundError if not found.
-   **/
-
-  static async get(handle) {
-    const sessionRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           WHERE handle = $1`,
-        [handle]);
-
-    const session = sessionRes.rows[0];
-
-    if (!session) throw new NotFoundError(`No session: ${handle}`);
-
-    const jobsRes = await db.query(
-          `SELECT id, title, salary, equity
-           FROM jobs
-           WHERE company_handle = $1
-           ORDER BY id`,
-        [handle],
-    );
-
-    session.jobs = jobsRes.rows;
-
-    return session;
-  }
-
-  /** Update company data with `data`.
-   *
-   * This is a "partial update" --- it's fine if data doesn't contain all the
-   * fields; this only changes provided ones.
-   *
-   * Data can include: {name, description, numEmployees, logoUrl}
-   *
-   * Returns {handle, name, description, numEmployees, logoUrl}
-   *
-   * Throws NotFoundError if not found.
-   */
-
-  static async update(handle, data) {
-    const { setCols, values } = sqlForPartialUpdate(
-        data,
-        {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url",
-        });
-    const handleVarIdx = "$" + (values.length + 1);
-
-    const querySql = `UPDATE companies 
-                      SET ${setCols} 
-                      WHERE handle = ${handleVarIdx} 
-                      RETURNING handle, 
-                                name, 
-                                description, 
-                                num_employees AS "numEmployees", 
-                                logo_url AS "logoUrl"`;
-    const result = await db.query(querySql, [...values, handle]);
-    const company = result.rows[0];
-
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
-
-    return company;
-  }
-
-  /** Delete given company from database; returns undefined.
-   *
-   * Throws NotFoundError if company not found.
-   **/
-
-  static async remove(handle) {
-    const result = await db.query(
-          `DELETE
-           FROM companies
-           WHERE handle = $1
-           RETURNING handle`,
-        [handle]);
-    const company = result.rows[0];
-
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
-  }
 }
 
 
